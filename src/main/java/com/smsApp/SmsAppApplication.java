43c631a8429798a896ddf13fc57d3b6af878b294
nisha555 <nishakothare143@gmail.com>
2023-01-01 18:26:48 +0530

    smsapp backup

---
+++ src/main/java/com/smsApp/SmsAppApplication.java
@@ -0,0 +1,13 @@
+package com.smsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/smsApp/SmsService.java
@@ -0,0 +1,33 @@
+package com.smsApp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+
+@Component
+public class SmsService {
+
+	    
+	    private final String ACCOUNT_SID ="ACba0d1dda2171308db602199a3b8d51b4";
+
+	    private final String AUTH_TOKEN = "befe250a1d712ccd1c33d1526fd5ae93";
+
+	    private final String FROM_NUMBER = "+14452694990";
+
+	    public void send(SmsPojo sms) {
+	    	Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+	        Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(FROM_NUMBER), sms.getMessage())
+	                .create();
+	        System.out.println("here is my id:"+message.getSid());// Unique resource ID created to manage this transaction
+
+	    }
+
+	    public void receive(MultiValueMap<String, String> smscallback) {
+	    }
+	
+}
